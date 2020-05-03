9f058086a092ea4e3e45453bf80c303bf676020a
NeonIngram <298283427@qq.com>
2020-05-03 23:21:55 +0800

    homework

--- assignment/todolist/app/src/main/java/com/byted/camp/todolist/MainActivity.java
+++ assignment/todolist/app/src/main/java/com/byted/camp/todolist/MainActivity.java
@@ -1,8 +1,12 @@
 package com.byted.camp.todolist;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -13,21 +17,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.util.Log;
 
 import com.byted.camp.todolist.beans.Note;
+import com.byted.camp.todolist.beans.State;
+import com.byted.camp.todolist.db.TodoDbHelper;
+import com.byted.camp.todolist.db.TodoContract.TodoEntry;
 import com.byted.camp.todolist.operation.activity.DatabaseActivity;
 import com.byted.camp.todolist.operation.activity.DebugActivity;
 import com.byted.camp.todolist.operation.activity.SettingActivity;
 import com.byted.camp.todolist.ui.NoteListAdapter;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="Main";
 
     private static final int REQUEST_CODE_ADD = 1002;
 
     private RecyclerView recyclerView;
     private NoteListAdapter notesAdapter;
+
+    private TodoDbHelper dbhelper =new TodoDbHelper(this);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,15 +129,97 @@ public class MainActivity extends AppCompatActivity {
 
     private List<Note> loadNotesFromDatabase() {
         // TODO 从数据库中查询数据，并转换成 JavaBeans
-        return null;
+        //return null;
+        List<Note> list = new ArrayList<>();
+        SQLiteDatabase db = dbhelper.getReadableDatabase();
+        String[] projection = {
+                BaseColumns._ID,
+                TodoEntry.COLUMN_CONTENT,
+                TodoEntry.COLUMN_DATE,
+                TodoEntry.COLUMN_STATE,
+                TodoEntry.COLUMN_PRIORITY
+        };
+        String sortOrder =
+                TodoEntry.COLUMN_PRIORITY + " ASC";
+        Cursor cursor = db.query(
+                TodoEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                null,              // The columns for the WHERE clause
+                null,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
+
+        Log.i(TAG, "perfrom query data:");
+        while (cursor.moveToNext()) {
+            long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(TodoEntry._ID));
+            String content = cursor.getString(cursor.getColumnIndex(TodoEntry.COLUMN_CONTENT));
+            String date = cursor.getString(cursor.getColumnIndex(TodoEntry.COLUMN_DATE));
+            int state = cursor.getInt(cursor.getColumnIndex(TodoEntry.COLUMN_STATE));
+            int priority = cursor.getInt(cursor.getColumnIndex(TodoEntry.COLUMN_PRIORITY));
+            Log.i(TAG, "itemId:" + itemId + ", Content:" + content + ", Date:" + date+", State:"+state+", Priority: "+priority);
+
+            Note note = new Note(itemId);
+            note.setContent(content);
+            try{
+                String format ="E, dd MMM yyyy HH:mm:ss";
+                SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.ENGLISH);
+                Date date_ = dateFormat.parse(date);
+                note.setDate(date_);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            note.setState(State.from(state));
+            note.setPriority(priority);
+            list.add(note);
+        }
+        cursor.close();
+
+
+        return list;
     }
 
     private void deleteNote(Note note) {
         // TODO 删除数据
+        SQLiteDatabase db = dbhelper.getWritableDatabase();
+        String selection = TodoEntry._ID + " = ?";
+        String[] selectionArgs = {String.valueOf(note.id)};
+        // Issue SQL statement.
+        int deletedRows = db.delete(TodoEntry.TABLE_NAME, selection, selectionArgs);
+        Log.i(TAG, "perform delete data, result:" + deletedRows);
     }
 
     private void updateNode(Note note) {
         // 更新数据
+        SQLiteDatabase db = dbhelper.getWritableDatabase();
+
+        // New value for one column
+
+        // Which row to update, based on the title
+        String selection = TodoEntry._ID + " = ?";
+        String[] selectionArgs = {String.valueOf(note.id)};
+
+        State state = note.getState();
+        ContentValues values=new ContentValues();
+
+        if(state==State.TODO)
+        {
+            values.put(TodoEntry.COLUMN_STATE,0);
+        }
+        else
+        {
+            values.put(TodoEntry.COLUMN_STATE,1);
+        }
+
+        int count = db.update(
+                TodoEntry.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs);
+        Log.i(TAG, "perform update data, result:" + count);
     }
 
 }
--- assignment/todolist/app/src/main/java/com/byted/camp/todolist/NoteActivity.java
+++ assignment/todolist/app/src/main/java/com/byted/camp/todolist/NoteActivity.java
@@ -1,7 +1,9 @@
 package com.byted.camp.todolist;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -9,12 +11,30 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.byted.camp.todolist.db.TodoContract.TodoEntry;
+import com.byted.camp.todolist.db.TodoDbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class NoteActivity extends AppCompatActivity {
 
+    private static final String TAG = "Note";
+
     private EditText editText;
     private Button addBtn;
+
+    private TodoDbHelper dbhelper = new TodoDbHelper(this);
+    private RadioButton btn1;
+    private RadioButton btn2;
+    private RadioButton btn3;
+    private RadioGroup radiogroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +74,10 @@ public class NoteActivity extends AppCompatActivity {
                 finish();
             }
         });
+        radiogroup=findViewById(R.id.radiogroup);
     }
+
+
 
     @Override
     protected void onDestroy() {
@@ -63,6 +86,51 @@ public class NoteActivity extends AppCompatActivity {
 
     private boolean saveNote2Database(String content) {
         // TODO 插入一条新数据，返回是否插入成功
-        return false;
+        //return false;
+       SQLiteDatabase db = dbhelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(TodoEntry.COLUMN_CONTENT,content);
+        Date date = new Date();
+        String format = "E, dd MMM yyyy HH:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.ENGLISH);
+        values.put(TodoEntry.COLUMN_DATE, dateFormat.format(date));
+        values.put(TodoEntry.COLUMN_STATE, 0);
+        //int count = radiogroup.getChildCount();
+       /* for(int i=0;i<count;i++)
+        {
+            RadioButton rb = (RadioButton) radiogroup.getChildAt(i);
+            if (rb.isChecked()) {
+                values.put(TodoEntry.COLUMN_PRIORITY, i);
+                break;
+            }
+
+        }
+
+       if(btn1.isChecked()) {
+           values.put(TodoEntry.COLUMN_PRIORITY,1);
+       }
+       else if(btn2.isChecked()){
+           values.put(TodoEntry.COLUMN_PRIORITY,2);
+       }
+       else{
+           values.put(TodoEntry.COLUMN_PRIORITY,3);
+       }*/
+        int count = radiogroup.getChildCount();
+        for (int i = 0; i < count; i++) {
+            RadioButton rb = (RadioButton) radiogroup.getChildAt(i);
+            if (rb.isChecked()) {
+                values.put(TodoEntry.COLUMN_PRIORITY, i);
+                break;
+            }
+        }
+
+        long newRowId = db.insert(TodoEntry.TABLE_NAME, null, values);
+        if (newRowId < 0)
+            return false;
+        else
+            return true;
     }
+
+
 }
